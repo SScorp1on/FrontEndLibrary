3d6e0d3bd65f63dc5b0b3ab5f1b044eb88279acc
SScorp1on <german4941@gmail.com>
2022-03-21 01:00:57 +0100

    version 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { OsobaZoznamComponent } from './osoba/osoba-zoznam/osoba-zoznam.component';
+import { OsobaFormularComponent } from './osoba/osoba-formular/osoba-formular.component';
+import {AppRoutingModule} from './app-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import {RouterModule} from '@angular/router';
+import { OsobaStrankaComponent } from './osoba/osoba-stranka/osoba-stranka.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OsobaZoznamComponent,
+    OsobaFormularComponent,
+    MenuComponent,
+    OsobaStrankaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
